452886ee87905d35304117a6842073847849c948
hpk369 <keshruwalaharsh@gmail.com>
2025-03-24 13:12:34 -0400

    Setup Firebase and Firebase Store

--- ec_app/lib/main.dart
+++ ec_app/lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 //import '';
 
-void main() {
+Future<void> main() async {
 
   // Todo: Add Widgets Binding
   // Todo: Init Local Storage
   // Todo: Await Native Splash
   // Todo: Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then(
+      (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
   // Todo: Initialize Authentication
 
+  // Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
